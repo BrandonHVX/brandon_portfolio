9a26a0299d7fd2111b60e078fd8742928cc1d11b
BrandonHVX <brandongines@gmail.com>
2018-12-02 15:36:30 -0500

    home-page

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render(){
+        return(
+    <div>
+
+
+        <h1>BRANDON GINES</h1>
+        <p className="title">
+            Motivated individual experienced in video production. Interested in videographer/editor
+            position with potential for advancement and the ability to utilize and enhance existing skills. 
+            Skilled in cross-functional and team collaboration for preparation of interviews, commercials, 
+            short films and music videos with the use of interpersonal and customer services skills to enhance 
+            client experience. 
+
+        
+        
+        </p>
+
+
+
+
+    </div>
+        )
+    }
+}
+
+export default Home ;
